05c953edba62b25bf3d74f6791fe69759f048208
nestordavalos <nestordavalos@gmail.com>
2024-06-05 12:49:09 -0400

    fix

--- backend/src/libs/wbot.ts
+++ backend/src/libs/wbot.ts
@@ -53,9 +53,7 @@ export const initWbot = async (whatsapp: Whatsapp): Promise<Session> => {
 
       const wbot: Session = new Client({
         session: sessionCfg,
-        authStrategy: new LocalAuth({
-          dataPath: "sessions",
-          }),
+        authStrategy: new LocalAuth({ clientId: `bd_${whatsapp.id}` }),
         //authStrategy: new LocalAuth({ clientId: `bd_${whatsapp.id}` }),
         restartOnAuthFail: false,
         webVersionCache: {
